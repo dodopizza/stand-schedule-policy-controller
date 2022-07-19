fd9009c226b6b35fd3623fef03472331bd5a2acd
ArtemTrofimushkin <artemtrofimushkin@gmail.com>
2022-07-19 19:37:58 +0300

    chore: cleanup codegen script
    Signed-off-by: ArtemTrofimushkin <artemtrofimushkin@gmail.com>

--- hack/run-codegen.sh
+++ hack/run-codegen.sh
@@ -5,62 +5,73 @@ set -o nounset
 set -o pipefail
 
 SCRIPT_ROOT="$(cd "$(dirname "${BASH_SOURCE}")"/.. && pwd)"
-CONTROLLER_GEN_REQ_VERSION="v0.9.1-0.20220629131006-1878064c4cdf"
-CONTROLLER_GEN_TMP_DIR=$(mktemp -d)
-pushd "$CONTROLLER_GEN_TMP_DIR" >/dev/null
-  go install sigs.k8s.io/controller-tools/cmd/controller-gen@$CONTROLLER_GEN_REQ_VERSION
-popd >/dev/null
-rm -rf "$CONTROLLER_GEN_TMP_DIR"
-CONTROLLER_GEN=${GOPATH}/bin/controller-gen
 
-export GO111MODULE="on"
-export GOFLAGS="-mod=readonly"
-export GOPATH="$(mktemp -d)"
-export PROJECT_NAME="github.com/dodopizza/stand-schedule-policy-controller"
+echo "Install controller-gen"
+go install \
+  "sigs.k8s.io/controller-tools/cmd/controller-gen@v0.9.1-0.20220629131006-1878064c4cdf"
+export CONTROLLER_GEN=${GOPATH}/bin/controller-gen
 
 # Even when modules are enabled, the code-generator tools always write to
 # a traditional GOPATH directory, so fake on up to point to the current
 # workspace.
-mkdir -p "$GOPATH/src/github.com/dodopizza"
+
+export GO111MODULE="on"
+export GOFLAGS="-mod=readonly"
+export GOPATH="$(mktemp -d)"
+export ORG_NAME="github.com/dodopizza"
+export PROJECT_NAME="$ORG_NAME/stand-schedule-policy-controller"
+
+mkdir -p "$GOPATH/src/$ORG_NAME"
 ln -s "${SCRIPT_ROOT}" "$GOPATH/src/$PROJECT_NAME"
 
-OUTPUT_PKG=$PROJECT_NAME/pkg/client
-APIS_PKG=$PROJECT_NAME/pkg
-CLIENTSET_NAME=versioned
-CLIENTSET_PKG_NAME=clientset
-COMMON_FLAGS="--go-header-file ${SCRIPT_ROOT}/hack/boilerplate.go.txt"
+# Package name with defined CRD types
+APIS_PKG=$PROJECT_NAME/pkg/apis/standschedules
+APIS_PKG_PRIMARY_VERSION=v1
+APIS_PKG_SUPPORTED_VERSIONS=(
+  v1
+)
 
-echo "Generating clientset at ${OUTPUT_PKG}/${CLIENTSET_PKG_NAME}"
+# Package name with generated clients (resource client, factories, informers, listers, etc)
+CLIENT_PKG=$PROJECT_NAME/pkg/client
+CLIENT_CLIENTSET_PKG="${CLIENT_PKG}/clientset"
+CLIENT_CLIENTSET_VERSIONED_PKG_NAME=versioned
+CLIENT_LISTERS_PKG="${CLIENT_PKG}/listers"
+CLIENT_INFORMERS_PKG="${CLIENT_PKG}/informers"
+
+# Flags that shared between various generators
+GENERATOR_FLAGS="--go-header-file ${SCRIPT_ROOT}/hack/boilerplate.go.txt"
+
+echo "Generating ClientSet at ${CLIENT_CLIENTSET_PKG}"
 go run k8s.io/code-generator/cmd/client-gen \
-  --clientset-name "${CLIENTSET_NAME}" \
+  --clientset-name "${CLIENT_CLIENTSET_VERSIONED_PKG_NAME}" \
   --input-base "" \
-  --input "${APIS_PKG}/apis/standschedules/v1" \
-  --output-package "${OUTPUT_PKG}/${CLIENTSET_PKG_NAME}" \
-  ${COMMON_FLAGS}
+  --input "${APIS_PKG}/${APIS_PKG_PRIMARY_VERSION}" \
+  --output-package "${CLIENT_CLIENTSET_PKG}" \
+  ${GENERATOR_FLAGS}
 
-echo "Generating listers at ${OUTPUT_PKG}/listers"
+echo "Generating listers at ${CLIENT_LISTERS_PKG}"
 go run k8s.io/code-generator/cmd/lister-gen \
-  --input-dirs "${APIS_PKG}/apis/standschedules/v1" \
-  --output-package "${OUTPUT_PKG}/listers" \
-  ${COMMON_FLAGS}
+  --input-dirs "${APIS_PKG}/${APIS_PKG_PRIMARY_VERSION}" \
+  --output-package "${CLIENT_LISTERS_PKG}" \
+  ${GENERATOR_FLAGS}
 
-echo "Generating informers at ${OUTPUT_PKG}/informers"
+echo "Generating informers at ${CLIENT_INFORMERS_PKG}"
 go run k8s.io/code-generator/cmd/informer-gen \
-  --input-dirs "${APIS_PKG}/apis/standschedules/v1" \
-  --versioned-clientset-package "${OUTPUT_PKG}/${CLIENTSET_PKG_NAME}/${CLIENTSET_NAME}" \
-  --listers-package "${OUTPUT_PKG}/listers" \
-  --output-package "${OUTPUT_PKG}/informers" \
-  ${COMMON_FLAGS}
+  --input-dirs "${APIS_PKG}/${APIS_PKG_PRIMARY_VERSION}" \
+  --versioned-clientset-package "${CLIENT_CLIENTSET_PKG}/${CLIENT_CLIENTSET_VERSIONED_PKG_NAME}" \
+  --listers-package "$CLIENT_LISTERS_PKG" \
+  --output-package "${CLIENT_INFORMERS_PKG}" \
+  ${GENERATOR_FLAGS}
 
-for VERSION in v1
+for VERSION in "${APIS_PKG_SUPPORTED_VERSIONS[@]}"
 do
-  echo "Generating ${VERSION} register at ${APIS_PKG}/apis/standschedules/${VERSION}"
+  echo "Generating ${VERSION} register at ${APIS_PKG}/${VERSION}"
   go run k8s.io/code-generator/cmd/register-gen \
-    --input-dirs "${APIS_PKG}/apis/standschedules/${VERSION}" \
-    --output-package "${APIS_PKG}/apis/standschedules/${VERSION}" \
-    ${COMMON_FLAGS}
+    --input-dirs "${APIS_PKG}/${VERSION}" \
+    --output-package "${APIS_PKG}/${VERSION}" \
+    ${GENERATOR_FLAGS}
 
-  echo "Generating ${VERSION} deepcopy at ${APIS_PKG}/apis/standschedules/${VERSION}"
+  echo "Generating ${VERSION} deepcopy at ${APIS_PKG}/${VERSION}"
   $CONTROLLER_GEN object:headerFile="${SCRIPT_ROOT}/hack/boilerplate.go.txt" \
-    paths="${APIS_PKG}/apis/standschedules/${VERSION}"
+    paths="${APIS_PKG}/${VERSION}"
 done
